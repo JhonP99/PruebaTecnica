cd251abaab5a7762a6df6e8c6670521e4225bcd6
Jhon <jhon.paz0724@jala.university>
2026-06-26 11:32:17 -0500

    chore(back): initialize Spring Boot project

---
+++ back/opticsP/src/main/java/com/optics/opticsP/PruebaBack.java
@@ -0,0 +1,13 @@
+package com.optics.opticsP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBack {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBack.class, args);
+	}
+
+}
